8992481d1c61ff99909303c52b25af599bcf1545
Janine Reidenbach <janine.reidenbach@fom-net.de>
2022-12-23 09:39:55 +0100

    Automat3 läuft richtig

--- 03.24/Automat3.c
+++ 03.24/Automat3.c
@@ -7,8 +7,13 @@ int main()
 	int Auswahl = 0, Anzahl = 0;
 	float Warenwert = 0, Einwurf = 0, Einzelpreis = 0;
 
-	printf("\nGetränkeautomat 3.0 | Bitte wählen Sie Ihr Getränk aus: \n");  
-	printf(" 1) Wasser (0.50 €)\n 2) Limo (1.00 €)\n 3) Bier (2.00 €)\n\n Geben Sie 1, 2 oder 3 ein: ");
+	printf("\n -------------------------------------------------- \n");
+	printf("\n Getränkeautomat 3.0 | Bitte wählen Sie Ihr Getränk aus: \n");  
+	printf(" 1) Wasser (0.50 €)\n 2) Limo (1.00 €)\n 3) Bier (2.00 €)\n");
+	
+	printf("\n -------------------------------------------------- \n");
+	printf("\n >>>>> Getränkeauswahl <<<<<\n");
+	printf("\n Geben Sie 1, 2 oder 3 ein: ");
 	scanf("%d", &Auswahl);
 
 	if(Auswahl < 1 || Auswahl > 3) {
@@ -28,36 +33,43 @@ int main()
 			case 3: Einzelpreis = 2;
 			break;
 		}
+		printf("\n -------------------------------------------------- \n");
+       		printf("\n >>>>> Mengenauswahl <<<<<\n");
 
 		printf("\n Wie viele Flaschen? ");
 		scanf("%d", &Anzahl);
 
 		Warenwert = Anzahl * Einzelpreis;
+		
+		printf("\n -------------------------------------------------- \n");
+		printf("\n >>>>> Bezahlvorgang <<<<<\n");
 
-		printf("\n Bitte werfen Sie %.2f € in Münzen (0,50 €, 1 € oder 2 €) ein: ", Warenwert);
-		scanf("%f", &Einwurf);
+		while(Warenwert > 0) {
+
+			printf("\n Bitte werfen Sie %.2f € in Münzen (0,50 €, 1 € oder 2 €) ein: ", Warenwert);
+			scanf("%f", &Einwurf);
 			if(Einwurf == 1 || Einwurf == 0.50 || Einwurf == 2) {
 
-				while(Einwurf != Warenwert) {
 				Warenwert = Warenwert-Einwurf;
-				
-				printf("\n Es fehlen noch %.2f €, bitte werfen Sie noch eine Münze ein: ", Warenwert);
-				scanf("%f", &Einwurf);
 
-				}
-
-				if(Warenwert = Einwurf) {
-				printf("\n Vielen Dank, bitte entnehmen Sie das Getränk!\n\n");
-				}
 			}
-
+			
 			else {
-			printf("\n Falscher Einwurf! Sie erhalten jetzt Ihr Geld zurück und können nochmal starten.\n\n");
+				printf("\n Falscher Einwurf!\n");
 			}
-	}
 
-	else {
-		printf("\n Geben Sie bitte eine Zahl zwischen 1 - 3 ein!\n");
+			if(Warenwert <= 0) {
+				printf("\n Vielen Dank! \n");
+
+				printf("\n -------------------------------------------------- \n");
+				printf("\n >>>>> Flaschenausgabe <<<<< \n");
+				for(int i = 1; i <= Anzahl; i++) {
+					printf("\n Bitte entnehmen Sie Getränk %i von %d! \n", i, Anzahl);
+				}
+
+			}
+		} 
+		printf("\n Guten Durst und auf Wiedersehen! \n\n ------------------------------------------------\n\n");
 	}
 
 return 0;
